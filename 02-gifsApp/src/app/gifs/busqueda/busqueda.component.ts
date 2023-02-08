361fd093c8ba93c51028fba1f926358494ae3478
Luca <monteleone546@gmail.com>
2023-02-07 21:05:06 -0300

    Creando Gifs App

--- 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import { GifsService } from './../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,11 +9,20 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 export class BusquedaComponent {
 
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>; //ElementeRef es un tipo de dato, para decirle que proviene de un input se coloca en el generico <HTMLInputElement>
-  //ViewChild busca en el HTML un elemento que tenga una referencia local y lo va a asignar a una variable.
+  //ViewChild busca en el HTML un elemento que tenga una referencia local y lo va a asignar a una variable. (Lo que esta entre parentesis hace referencia a la variable local)
   //El signo de exclamacion luego de txtBuscar! es el operador non null assertion operator, es para decirle a TS que ese elemento siempre va a tener algo  
+  constructor( private gifsService: GifsService ) {}
+  
   buscar() {
+
     const valor = this.txtBuscar.nativeElement.value;
-    console.log( valor );
+
+    if (valor.trim().length === 0) {
+      return;
+    } 
+  
+    this.gifsService.buscarGifs(valor)
+
     this.txtBuscar.nativeElement.value = '';
   }
 
--- 02-gifsApp/src/app/gifs/gifs-page/gifs-page.component.ts
+++ 02-gifsApp/src/app/gifs/gifs-page/gifs-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-gifs-page',
   templateUrl: './gifs-page.component.html',
-  styleUrls: ['./gifs-page.component.css']
 })
 export class GifsPageComponent {
 
